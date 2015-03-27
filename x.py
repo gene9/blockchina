a2ded5c9fccf772388b1080c6cb5fb9e54941128
gene9 <gene9@example.com>
2015-03-27 12:52:34 -0500

    initial

---
+++ x.py
@@ -0,0 +1,12 @@
+
+print 'sudo iptables -F "chinadrop"'
+print 'sudo iptables -X "chinadrop"'
+
+print 'sudo iptables -N "chinadrop"'
+
+for line in open('cn.zone'):
+    line = line.strip()
+    if not line:
+        continue
+
+    print 'sudo iptables -A "chinadrop" -s {0} -j DROP'.format(line)
